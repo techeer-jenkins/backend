e77da362d13a1601ea3d2778f497d9a3fa77b310
Ryan <leehahnwoong@gmail.com>
2024-07-10 14:34:39 -0700

    feat: basic path

--- main.py
+++ main.py
@@ -20,8 +20,11 @@ def get_db():
     finally:
         db.close()
 
+@app.get("/api/v1/")
+def read_root():
+    return {"message": "Welcome to the FastAPI Todo API"}
 
-@app.post("api/v1/users/",response_model=schemas.User)
+@app.post("/api/v1/users/",response_model=schemas.User)
 def post_user(user:schemas.UserCreate, db:Session=Depends(get_db)):
     db_user = crud.get_user_by_email(db, email=user.email)
     if db_user:
@@ -29,13 +32,13 @@ def post_user(user:schemas.UserCreate, db:Session=Depends(get_db)):
     return crud.create_user(db=db,user=user)
 
 
-@app.get("api/v1/users/", response_model=list[schemas.User])
+@app.get("/api/v1/users/", response_model=list[schemas.User])
 def get_users(skip:int=0, limit:int=0, db:Session=Depends(get_db)):
     users = crud.get_users(db,skip=skip,limit=limit)
     return users
 
 
-@app.get("api/v1/users/{user_id}/",response_model=schemas.User)
+@app.get("/api/v1/users/{user_id}/",response_model=schemas.User)
 def get_user(user_id:int, db:Session=Depends(get_db)):
     db_user = crud.get_user(db,user_id =user_id )
     if db_user is None:
@@ -43,12 +46,12 @@ def get_user(user_id:int, db:Session=Depends(get_db)):
     return db_user
 
 
-@app.post("api/v1/users/{user_id}/todos/",response_model=schemas.Todo)
+@app.post("/api/v1/users/{user_id}/todos/",response_model=schemas.Todo)
 def post_todo_for_user(user_id:int, todo:schemas.TodoCreate, db:Session=Depends(get_db)):
     return crud.create_user_todo(db=db,user_id=user_id, todo=todo)
 
 
-@app.get("api/v1/todos/", response_model=list[schemas.Todo])
+@app.get("/api/v1/todos/", response_model=list[schemas.Todo])
 def get_todos(skip:int=0,limit:int=100,db:Session=Depends(get_db)):
     todos = crud.get_todos(db,skip=skip,limit=limit)
     return todos

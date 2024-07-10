ede17533ec9db254b2bed54295562edb07d1c2cc
Ryan <leehahnwoong@gmail.com>
2024-07-10 14:29:12 -0700

    fix: db host

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://session_example:session_example@localhost:3306/session_db"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://session_example:session_example@db:3306/session_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL

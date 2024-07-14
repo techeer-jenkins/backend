16abd9982a5f7940207867d5d5c30be32cbbd42e
Ryan <leehahnwoong@gmail.com>
2024-07-13 21:10:16 -0700

    fix

--- todo_django/todo/views.py
+++ todo_django/todo/views.py
@@ -1,8 +1,10 @@
+# views.py
 from rest_framework import viewsets, status
 from rest_framework.response import Response
 from rest_framework.decorators import action
 from .models import User2, Todo2
 from .serializers import UserSerializer, TodoSerializer
+from .tasks import add_with_delay  # Import the Celery task
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User2.objects.all()
@@ -20,3 +22,15 @@ class UserViewSet(viewsets.ModelViewSet):
 class TodoViewSet(viewsets.ModelViewSet):
     queryset = Todo2.objects.all()
     serializer_class = TodoSerializer
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            headers = self.get_success_headers(serializer.data)
+            
+            # Call the Celery task asynchronously
+            result = add_with_delay.apply_async((1, 2))
+            
+            return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
